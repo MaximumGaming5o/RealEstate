8c2e2e62ab833a51dabc076000044ac773b1f259
Adam Zyluk <35942983+MaximumGaming5o@users.noreply.github.com>
2024-11-20 02:02:52 -0500

    Created Database with sample data

--- RealEstate/Program.cs
+++ RealEstate/Program.cs
@@ -1,3 +1,57 @@
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+//Create a link to MongoDB
+const string connectionUri = "mongodb+srv://AdamZ:xT83uhvWICmqumdr@realestate.kx91f.mongodb.net/?retryWrites=true&w=majority&appName=RealEstate";
+var settings = MongoClientSettings.FromConnectionString(connectionUri);
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+
+// Create a new client and connect to the server
+var mongoClient = new MongoClient(settings);
+
+// Send a ping to confirm a successful connection
+try
+{
+    var result = mongoClient.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+    Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+//Get the sample database with sample data for testing
+var myDB = mongoClient.GetDatabase("sample_mflix");
+
+//Create a list of collections on the database
+List<string> GetCollections()
+{
+    List<string> collections = new List<string>();
+
+    foreach (BsonDocument collection in myDB.ListCollectionsAsync().Result.ToListAsync<BsonDocument>().Result)
+    {
+        string name = collection["name"].AsString;
+        collections.Add(name);
+    }
+
+    return collections;
+}
+
+//Print list of collections
+for (int i = 0; i < GetCollections().Count; i++)
+{
+    Console.WriteLine(GetCollections()[i]);
+}
+
+
+
+
+
+
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
